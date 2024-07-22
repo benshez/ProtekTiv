4758e07f94d5e91a14b2683a941a40d917899643
Ben Van Heerden <benvh@youi.com>
2024-07-22 16:20:31 +1000

    Added GetAllAsync

--- backends/Core/ProtekTiv.Core.Interfaces/Services/IApiService.cs
+++ backends/Core/ProtekTiv.Core.Interfaces/Services/IApiService.cs
@@ -3,6 +3,7 @@
 public interface IApiService<T>
 {
     Task<T> GetAsync(string url);
+    Task<T> GetAllAsync<TRequest>(IEnumerable<TRequest> request);
     Task<T> PostAsync(string url, T item);
     Task<T> PutAsync(string url, T item);
     Task DeleteAsync(string url);
--- backends/Core/ProtekTiv.Core.Services/ApiService/Service.cs
+++ backends/Core/ProtekTiv.Core.Services/ApiService/Service.cs
@@ -2,29 +2,36 @@
 
 namespace ProtekTiv.Core.Services.ApiService;
 
-public class Service<T>(HttpClient httpClient) : IApiService<T>
+public class Service<TResponse>(HttpClient httpClient) : IApiService<TResponse>
 {
     private readonly HttpClient _httpClient = httpClient;
 
-    public async Task<T> GetAsync(string url)
+    public async Task<TResponse> GetAsync(string url)
     {
         HttpResponseMessage response = await _httpClient.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsAsync<T>();
+        return await response.Content.ReadAsAsync<TResponse>();
     }
 
-    public async Task<T> PostAsync(string url, T item)
+    public async Task<TResponse> GetAllAsync<TRequest>(IEnumerable<TRequest> request)
+    {
+        var tasks = request.Select(_ => _httpClient.GetAsync(_?.ToString()));
+        IEnumerable<HttpResponseMessage> response = await Task.WhenAll(tasks);
+        return (TResponse)response;
+    }
+
+    public async Task<TResponse> PostAsync(string url, TResponse item)
     {
         HttpResponseMessage response = await _httpClient.PostAsJsonAsync(url, item);
         response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsAsync<T>();
+        return await response.Content.ReadAsAsync<TResponse>();
     }
 
-    public async Task<T> PutAsync(string url, T item)
+    public async Task<TResponse> PutAsync(string url, TResponse item)
     {
         HttpResponseMessage response = await _httpClient.PutAsJsonAsync(url, item);
         response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsAsync<T>();
+        return await response.Content.ReadAsAsync<TResponse>();
     }
 
     public async Task DeleteAsync(string url)
